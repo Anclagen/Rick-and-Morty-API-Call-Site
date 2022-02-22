1ea31a8baa5a7c6463c49e13d3499531bf90d431
Anclagen <91462616+Anclagen@users.noreply.github.com>
2022-02-22 11:34:44 +0100

    fixed email validation

--- js/contact.js
+++ js/contact.js
@@ -10,6 +10,12 @@ const address = document.querySelector("#address");
 const addressError = document.querySelector(".error-address");
 const successContainer = document.querySelector(".success");
 
+fullname.value = "";
+email.value = "";
+subject.value = "";
+address.value = "";
+
+
 function checkLength(value, length) {
   if (value.trim().length > length) {
       return true;
@@ -19,34 +25,43 @@ function checkLength(value, length) {
 }
 
 function validateEmail(email) {
-  //https://digitalfortress.tech/tips/top-15-commonly-used-regex/
-  const emailRegEx = /\S+@\S+\.\S+/;
-  const validateEmail = emailRegEx.test(email);
-  console.log(validateEmail)
+  //https://digitalfortress.tech/tips/top-15-commonly-used-regex/ uncommon email check.
+  const emailRegEx = /^([a-z0-9_\.\+-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+  const validateEmail = emailRegEx.test(email.value);
   return validateEmail;
 }
 
 function validateContactForm(submission){
   submission.preventDefault();
+
+  //check runs through all if statements if one is false success container not displayed.
   let successCheck = true;
+
+  //clear success container if resubmitted
+  successContainer.innerHTML = "";
+  successContainer.classList.remove("main-item");
+
   if (!checkLength(fullname.value, 0)) {
     nameError.innerHTML = `Please enter you name.`;
     successCheck = false;
   } else{
     nameError.innerHTML = "";
   }
+
   if (!validateEmail(email)) {
     emailError.innerHTML = `Please enter a valid email address`;
     successCheck = false;
   } else{
     emailError.innerHTML = "";
   }
+
   if (!checkLength(subject.value, 9)) {
     subjectError.innerHTML = `Your subject must have a minium of 10 characters.`;
     successCheck = false;
   } else{
     subjectError.innerHTML = "";
   }
+
   if (!checkLength(address.value, 24)) {
     addressError.innerHTML = `Your address must have a minimum of 25 characters.`;
     successCheck = false;
@@ -54,10 +69,12 @@ function validateContactForm(submission){
     addressError.innerHTML = "";
   }
 
+  //if all validation passes form submitted and success div displayed 
   if(successCheck) {
     successContainer.classList.add("main-item")
     successContainer.innerHTML = "Success, your query has been submitted."
   }
 }
 
+//on submit run validation check
 form.addEventListener("submit", validateContactForm);
